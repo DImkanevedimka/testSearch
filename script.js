f8765e3608e9abf2b3cbdac8f31f74d4eaf5e5c4
DImkanevedimka <Dimkanevedimka33@gmail.com>
2017-10-04 14:14:14 +0300

    basic styles

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 $(function() {
     var suggestions = [
     {
-        value: 'test',
+        value: 'Product Name1',
         data: {
-            img: 'http://nakleykiavto.ru.images.1c-bitrix-cdn.ru/upload/iblock/5f7/5f7cb8d0f3d99084e0727425fd5884de.png?1448436989220428',
-            country: 'usa'
+            img: 'http://nakleykiavto.ru.images.1c-bitrix-cdn.ru/upload/resize_cache/iblock/e62/285_480_136a784aa5facf479774d3d75ef15b257/e628c15bcf9945269152465aac06e717.png?145324223391239',
+            Category: 'kak bi pivo',
+            price: '122 kr.',
+            link: '#'
         }
     },
     {
-        value: 'atest',
+        value: 'Product Name2',
         data: {
-            img: 'http://nakleykiavto.ru.images.1c-bitrix-cdn.ru/upload/iblock/5f7/5f7cb8d0f3d99084e0727425fd5884de.png?1448436989220428',
-            country: 'ukraine'
+            img: 'http://nakleykiavto.ru.images.1c-bitrix-cdn.ru/upload/resize_cache/iblock/803/285_480_136a784aa5facf479774d3d75ef15b257/803073d0d3d6b1d2cd3f1447e6617e9d.png?145323215699622',
+            Category: 'po4tipivo',
+            price: '113 kr.',
+            link: '#'
         }
     },
     {
-        value: 'dtest',
+        value: 'Product Name3',
         data: {
             img: 'http://nakleykiavto.ru.images.1c-bitrix-cdn.ru/upload/iblock/5f7/5f7cb8d0f3d99084e0727425fd5884de.png?1448436989220428',
-            country: 'uk'
+            Category: 'minion',
+            price: '112 kr.',
+            link: '#'
         }
     }
     ]
@@ -28,17 +34,20 @@ $(function() {
         lookup: suggestions,
         formatResult: function(sug, cur) {
             var img = [
-            '<img class="testimg", src=',
+            '<img class="productImg", src=',
             sug.data.img,
             '/>'
             ].join('');
 
-            var country = [
-            '<span>' + sug.data.country +
-            '</span>'
-            ]
-
-            return sug.value + img + country;
+            
+            var productName = '<div class="productName">' + sug.value + '</div>';
+            var productCategory = '<div class="productCategory">' + sug.data.Category + '</div>';
+            var productText ='<div class="productText">' + productName + productCategory + '</div>';
+            var productInfo = '<div class="productInfo">' + img + productText + '</div>';
+            var Price = '<div class="productPrice">'+ '<span>fra </span>' + sug.data.price + '</div>';
+            var full = '<a class="productFull" href='+ sug.data.link + '>' + productInfo + Price + '</a>';
+            
+            return full;
         }
     })
 });
