1d912488406c1808ae640ceb2d67407eb5d901a3
valera_korovelkov <vkorovelkov@perceptionbox.io>
2017-10-05 16:26:55 +0300

    Added suggestions json

--- script.js
+++ script.js
@@ -1,65 +1,12 @@
 $(function() {
-    var suggestions = [
-        {
-            value: 'Product default',
-            data: {
-                type: 'default',
-                Category: 'default',
-                price: '122 kr.',
-                link: '#'
-            }
-        },
-        {
-            value: 'Product Name1',
-            data: {
-                type: 'product',
-                img: 'http://nakleykiavto.ru.images.1c-bitrix-cdn.ru/upload/resize_cache/iblock/e62/285_480_136a784aa5facf479774d3d75ef15b257/e628c15bcf9945269152465aac06e717.png?145324223391239',
-                Category: 'product category',
-                price: '122 kr.',
-                link: '#'
-            }
-        },
-        {
-            value: 'Product Name2',
-            data: {
-                type: 'product',
-                img: 'http://nakleykiavto.ru.images.1c-bitrix-cdn.ru/upload/resize_cache/iblock/803/285_480_136a784aa5facf479774d3d75ef15b257/803073d0d3d6b1d2cd3f1447e6617e9d.png?145323215699622',
-                Category: 'product category',
-                price: '113 kr.',
-                link: '#'
-            }
-        },
-        {
-            value: 'Product Name3',
-            data: {
-                type: 'product',
-                img: 'http://nakleykiavto.ru.images.1c-bitrix-cdn.ru/upload/iblock/5f7/5f7cb8d0f3d99084e0727425fd5884de.png?1448436989220428',
-                Category: 'product category',
-                price: '112 kr.',
-                link: '#'
-            }
-        },
-        {
-            value: 'Show all suggestions',
-            data: {
-                type: 'product',
-                showAll: true,
-                link: '#',
-                suggestionsLenght: '3',
-            }
-        }
-    ]
-    
-
     $('#inputSearch').autocomplete({
         showNoSuggestionNotice: true,
-        lookup: suggestions,
+        serviceUrl: 'suggestions.json',
         beforeRender: function(container) {
             container.find('.autocomplete-suggestion .product').parent().wrapAll('<div class="suggestions__product"/>');
             container.find('.autocomplete-suggestion .default').parent().wrapAll('<div class="suggestions__default"/>');
         }, 
         formatResult: function(sug, cur) {
-
             if (sug.data.showAll) {
                 var showAll = '<a class="showAll" href=' + sug.data.link +  '>' + sug.value +" "+ '(' + sug.data.suggestionsLenght + ')</a>';
                 return showAll;
